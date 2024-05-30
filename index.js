283e687ed6941de5f1e782e136013f70e70ca84a
walala26 <walala26@gmail.com>
2024-05-29 21:15:36 -0500

    actualizacion codigo, index, app, vercel, packet json

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
- import app from "./src/app.js"
+import app from "./src/app.js"
 
- app.listen(app.get("port"))
+app.listen(app.get("port"))
 
-
- console.log("Server on Port", app.get("port"))
+console.log("Server on port", app.get("port"))

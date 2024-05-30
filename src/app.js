880ec6adad21276c7207f7f3638550746d72977f
walala26 <walala26@gmail.com>
2024-05-29 21:01:46 -0500

    Modificacion start en package.json

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express ()
 app.set("port", process.env.PORT || 4000)
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.get ("/", (req, res) => {
     res.json({ "message" : "Welcome to my messages API"})
